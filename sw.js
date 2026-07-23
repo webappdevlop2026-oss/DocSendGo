65e34f8de0d324beb6113e1660e1991b0cf5412b
webappdevlop2026-oss <webappdevlop2026@gmail.com>
2026-07-23 22:38:52 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'docsendgo-v7-code-share';
+const CACHE_VERSION = 'docsendgo-v8-desktop-upload';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const OFFLINE_URL = '/offline.html';
